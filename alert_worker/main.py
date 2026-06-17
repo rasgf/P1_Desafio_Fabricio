e323a88346a98811d78e4d4093422d674673d91e
Ricardo Galvão <pattz.rg@hotmail.com>
2026-06-17 15:22:53 -0300

    fix: converte worker para web service para garantir gratuidade no Render

--- alert_worker/main.py
+++ alert_worker/main.py
@@ -1,17 +1,16 @@
 import time
-from strategies import AlertContext, ConsoleAlertStrategy
 import os
 import requests
-
-# Este é um Microsserviço independente (Worker)
-# Ele faz polling na API (Poderia ser RabbitMQ/Kafka, mas HTTP é viável para o deploy rápido)
+import threading
+from strategies import AlertContext, ConsoleAlertStrategy
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 API_URL = os.getenv("API_URL", "http://localhost:8000/leituras")
+PORT = int(os.getenv("PORT", 8080))
 
 def monitor():
     strategy = ConsoleAlertStrategy()
     alert_context = AlertContext(strategy)
-    
     print("Alert Worker Iniciado...")
     while True:
         try:
@@ -23,8 +22,19 @@ def monitor():
                         alert_context.execute_alert(leitura['dispositivo_id'], leitura['distancia_cm'])
         except Exception as e:
             print("Erro de conexão:", e)
-        
-        time.sleep(10) # Verifica a cada 10 segundos
+        time.sleep(10)
+
+class DummyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Worker is alive")
 
 if __name__ == "__main__":
-    monitor()
+    # Roda o monitor em uma thread separada
+    t = threading.Thread(target=monitor, daemon=True)
+    t.start()
+    
+    # Roda um servidor web fake apenas para o Render achar que e um Web Service e dar o plano Gratis
+    server = HTTPServer(("0.0.0.0", PORT), DummyHandler)
+    server.serve_forever()
